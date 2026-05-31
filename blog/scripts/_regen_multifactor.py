e19da64e494ca6faf475bdd5424c5ce5cfd9cf61
minnawomamorukaisei <minnawomamorukaisei@gmail.com>
2026-06-01 00:05:58 +0900

    Deployed 5be1fda with MkDocs version: 1.6.1

--- blog/scripts/_regen_multifactor.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.insert(0, r"C:\stock_analysis")
-exec(open(r"C:\stock_analysis\scripts\blog\05_multifactor_scoreboard_make_images.py", encoding="utf-8").read().split("if __name__")[0])
-df_raw = load_universe()
-df = add_factor_scores(df_raw)
-make_scoreboard_top20(df)
-print("ok: 01_scoreboard_top20.png")
-make_oil_refining_compare(df)
-print("ok: 05_oil_refining_factors.png")
